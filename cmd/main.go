f36f9e07e10fcce9a1f3e541afc50f554dd19698
秦朝勇 <qinchy@msn.com>
2023-03-04 11:52:11 +0800

    refactor second step

--- cmd/main.go
+++ cmd/main.go
@@ -3,21 +3,15 @@ package main
 import (
 	"context"
 	"crypto/tls"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/qinchy/hellogo/gin/globalvar"
 	myHandler "github.com/qinchy/hellogo/gin/handler"
-	"github.com/qinchy/hellogo/gin/types"
-	"github.com/qinchy/hellogo/pkg/proto"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
-	//"github.com/qinchy/hellogo/pkg/write"
-	//"github.com/qinchy/hellogo/pkg/scheduler"
-	//"github.com/qinchy/hellogo/pkg/write"
 )
 
 func main() {
@@ -31,146 +25,46 @@ func main() {
 	// 改写日志到控制台和文件
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 
-	//r := gin.Default()
-
 	globalvar.Route.GET("/ping", myHandler.Ping)
 
-	globalvar.Route.GET("/someJSON", func(c *gin.Context) {
-		data := map[string]interface{}{
-			"lang": "GO语言",
-			"tag":  "<br>",
-		}
+	globalvar.Route.GET("/somejson", myHandler.SomeJson)
 
-		// 输出 : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
-		c.AsciiJSON(http.StatusOK, data)
-	})
+	globalvar.Route.GET("/morejson", myHandler.MoreJson)
 
-	globalvar.Route.GET("/moreJSON", func(c *gin.Context) {
-		// 你也可以使用一个结构体
-		var msg struct {
-			Name    string `json:"user"`
-			Message string
-			Number  int
-		}
-		msg.Name = "Lena"
-		msg.Message = "hey"
-		msg.Number = 123
-		// 注意 msg.Name 在 JSON 中变成了 "user"
-		// 将输出：{"user": "Lena", "Message": "hey", "Number": 123}
-		c.JSON(http.StatusOK, msg)
-	})
+	globalvar.Route.GET("/somexml", myHandler.SomeXml)
 
-	globalvar.Route.GET("/someXML", func(c *gin.Context) {
-		c.XML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
-	})
+	globalvar.Route.GET("/someyaml", myHandler.SomeYaml)
 
-	globalvar.Route.GET("/someYAML", func(c *gin.Context) {
-		c.YAML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
-	})
-
-	globalvar.Route.GET("/someProtoBuf", func(c *gin.Context) {
-		reps := []int64{int64(1), int64(2)}
-		label := "test"
-		// protobuf 的具体定义写在 pkg/proto 文件中。
-		data := &proto.Test{
-			Label: &label,
-			Reps:  reps,
-		}
-		// 请注意，数据在响应中变为二进制数据
-		// 将输出被 proto.Test protobuf 序列化了的数据
-		c.ProtoBuf(http.StatusOK, data)
-	})
+	globalvar.Route.GET("/someprotobuf", myHandler.SomeProtoBuf)
 
 	globalvar.Route.LoadHTMLGlob("templates/**/*")
 
-	globalvar.Route.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
-		})
-	})
+	globalvar.Route.GET("/index", myHandler.Index)
 
-	globalvar.Route.GET("/posts/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{
-			"title": "Posts",
-		})
-	})
+	globalvar.Route.GET("/posts/index", myHandler.PostIndex)
 
-	globalvar.Route.GET("/users/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
-			"title": "Users",
-		})
-	})
+	globalvar.Route.GET("/users/index", myHandler.UsersIndex)
 
-	globalvar.Route.GET("/JSONP", func(c *gin.Context) {
-		data := map[string]interface{}{
-			"foo": "bar",
-		}
+	globalvar.Route.GET("/jsonp", myHandler.JsonP)
 
-		// /JSONP?callback=x
-		// 将输出：x({\"foo\":\"bar\"})
-		c.JSONP(http.StatusOK, data)
-	})
-
-	globalvar.Route.POST("/loginForm", func(c *gin.Context) {
-		// 你可以使用显式绑定声明绑定 multipart form：
-		// c.ShouldBindWith(&form, binding.Form)
-		// 或者简单地使用 ShouldBind 方法自动绑定：
-		var form types.LoginForm
-		// 在这种情况下，将自动选择合适的绑定
-		if c.ShouldBind(&form) == nil {
-			if form.User == "user" && form.Password == "password" {
-				c.JSON(200, gin.H{"status": "you are logged in"})
-			} else {
-				c.JSON(401, gin.H{"status": "unauthorized"})
-			}
-		}
-	})
+	globalvar.Route.POST("/loginform", myHandler.LoginForm)
 
 	// curl -k -X POST --form "name=qinchy" --form "address=hangzhou" --form "birthday=2013-04-27" --form "id=987fbc97-4bed-5078-9f07-9141ba07c9f3"  "https://localhost/bindForm"
-	globalvar.Route.POST("/bindForm", func(c *gin.Context) {
-		var person types.Person
-		// 如果是 `GET` 请求，只使用 `Form` 绑定引擎（`query`）。
-		// 如果是 `POST` 请求，首先检查 `content-type` 是否为 `JSON` 或 `XML`，然后再使用 `Form`（`form-data`）。
-		// 查看更多：https://github.com/gin-gonic/gin/blob/master/binding/binding.go#L88
-		if c.ShouldBind(&person) == nil {
-			log.Println(person.Name)
-			log.Println(person.Address)
-			log.Println(person.Birthday)
-			c.String(200, "Success")
-		} else {
-			c.String(400, "Error")
-		}
-	})
+	globalvar.Route.POST("/bindform", myHandler.BindForm)
 
 	// curl -k "https://localhost/getb?field_a=hello&field_b=world"
 	globalvar.Route.GET("/getb", myHandler.GetDataB)
+
 	// curl -k "https://localhost/getb?field_a=hello&field_c=world"
 	globalvar.Route.GET("/getc", myHandler.GetDataC)
+
 	// curl -k "https://localhost/getb?field_x=hello&field_d=world"
 	globalvar.Route.GET("/getd", myHandler.GetDataD)
 
 	// 绑定 JSON ({"user": "user", "password": "password"})
-	globalvar.Route.POST("/loginJSON", func(c *gin.Context) {
-		var json types.LoginForm
-		if err := c.ShouldBindJSON(&json); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		if json.User != "user" || json.Password != "password" {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
-	})
+	globalvar.Route.POST("/loginJSON", myHandler.LoginJson)
 
 	// 绑定 XML (
-	//	<?xml version="1.0" encoding="UTF-8"?>
-	//	<root>
-	//		<user>user</user>
-	//		<password>password</password>
-	//	</root>)
 	// curl --location 'https://localhost/loginXML' \
 	// --header 'Content-Type: application/xml' \
 	// --data '<?xml version="1.0" encoding="UTF-8"?>
@@ -178,142 +72,34 @@ func main() {
 	//	 <user>user</user>
 	//	 <password>password</password>
 	// </root>'
-	globalvar.Route.POST("/loginXML", func(c *gin.Context) {
-		var xml types.LoginForm
-		if err := c.ShouldBindXML(&xml); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	globalvar.Route.POST("/loginxml", myHandler.LoginXml)
 
-		if xml.User != "user" || xml.Password != "password" {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
-	})
-
-	globalvar.Route.POST("/form_post", func(c *gin.Context) {
-		message := c.PostForm("message")
-		nick := c.DefaultPostForm("nick", "anonymous")
-
-		c.JSON(200, gin.H{
-			"status":  "posted",
-			"message": message,
-			"nick":    nick,
-		})
-	})
+	globalvar.Route.POST("/postform", myHandler.PostForm)
 
 	// 提供 unicode 实体
-	globalvar.Route.GET("/json", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"html": "<b>Hello, world!</b>",
-		})
-	})
+	globalvar.Route.GET("/json", myHandler.Json)
 
-	globalvar.Route.GET("/SecureJSON", func(c *gin.Context) {
-		names := []string{"lena", "austin", "foo"}
-
-		// 将输出：while(1);["lena","austin","foo"]
-		c.SecureJSON(http.StatusOK, names)
-	})
+	globalvar.Route.GET("/SecureJson", myHandler.SecureJson)
 
 	// curl -k -X POST "https://localhost/post1?id=11&page=1"
-	globalvar.Route.POST("/post1", func(c *gin.Context) {
-		// 请求示例
-		// POST /post?id=1234&page=1 HTTP/1.1
-		// Content-Type: application/x-www-form-urlencoded
-		//name=manu&message=this_is_great
-
-		// 从url中获取字段
-		id := c.Query("id")
-		page := c.DefaultQuery("page", "0")
-		// 从表单中获取字段
-		name := c.PostForm("name")
-		message := c.PostForm("message")
-
-		fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
-		c.JSON(200, gin.H{
-			"id":      id,
-			"page":    page,
-			"name":    name,
-			"message": message,
-		})
-	})
+	globalvar.Route.POST("/postformwithquery", myHandler.PostFormWithQuery)
 
 	// 映射查询字符串或表单参数
 	// curl -k -X POST --location "https://localhost/post2?ids\[a\]=11&ids\[b\]=22" --header "Content-Type: application/x-www-form-urlencoded" -d "names[first]=thinkerou&names[second]=tianou"
-	globalvar.Route.POST("/post2", func(c *gin.Context) {
-		ids := c.QueryMap("ids")
-		names := c.PostFormMap("names")
-
-		log.Printf("ids: %v; names: %v", ids, names)
-		c.JSON(200, gin.H{
-			"ids":  ids,
-			"name": names,
-		})
-	})
+	globalvar.Route.POST("/postmultiformwithquery", myHandler.PostMultiFormWithQuery)
 
 	// 提供字面字符
-	globalvar.Route.GET("/purejson", func(c *gin.Context) {
-		// 这里的sleep是用来模拟优雅关机的，当请求来后，会处理完这个请求后再关闭服务器
-		time.Sleep(10 * time.Second)
-		c.PureJSON(200, gin.H{
-			"html": "<b>Hello, world!</b>",
-		})
-	})
+	globalvar.Route.GET("/purejson", myHandler.PureJson)
 
 	// 为 multipart forms 设置较低的内存限制 (默认是 32 MiB)
 	// curl -k -X POST https://localhost/singleupload  -F "file=@D:\Source_Code\go\src\github.com\qinchy\hellogo\cmd\main.go"   -H "Content-Type: multipart/form-data"
 	globalvar.Route.MaxMultipartMemory = 8 << 20 // 8 MiB
-	globalvar.Route.POST("/singleupload", func(c *gin.Context) {
-		// 单文件
-		file, _ := c.FormFile("file")
-		log.Println(file.Filename)
-
-		// 这里的当前目录是整个工程的路径，比如当前是$GOPATH/src/github.com/qinchy/hellogo
-		// 感觉这个目录是go.mod中module的路径
-		dst := "./" + file.Filename
-		log.Printf("dst:%v", dst)
-		// 上传文件至指定的完整文件路径
-		c.SaveUploadedFile(file, dst)
-
-		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
-	})
+	globalvar.Route.POST("/singleupload", myHandler.SingleUpload)
 
 	// curl -k -X POST https://localhost/multiupload  -F "upload[]=@C:\Users\Administrator\AppData\Local\Temp\GoLand\___go_build_github_com_qinchy_hellogo_cmd.exe"   -F "upload[]=@D:\Source_Code\go\bin\hellogo\go_build_github_com_qinchy_hellogo.exe"   -H "Content-Type: multipart/form-data"
-	globalvar.Route.POST("/multiupload", func(c *gin.Context) {
-		// Multipart form
-		form, _ := c.MultipartForm()
-		files := form.File["upload[]"]
+	globalvar.Route.POST("/multiupload", myHandler.MultiUpload)
 
-		for _, file := range files {
-			log.Println(file.Filename)
-
-			dst := "./" + file.Filename + ".bak"
-			// 上传文件至指定目录
-			c.SaveUploadedFile(file, dst)
-		}
-		c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
-	})
-
-	globalvar.Route.GET("/someDataFromReader", func(c *gin.Context) {
-		response, err := http.Get("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png")
-		if err != nil || response.StatusCode != http.StatusOK {
-			c.Status(http.StatusServiceUnavailable)
-			return
-		}
-
-		reader := response.Body
-		contentLength := response.ContentLength
-		contentType := response.Header.Get("Content-Type")
-
-		extraHeaders := map[string]string{
-			"Content-Disposition": `attachment; filename="gopher.png"`,
-		}
-
-		c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
-	})
+	globalvar.Route.GET("/fetchfromreader", myHandler.FetchFromReader)
 
 	//  =================使用 BasicAuth 中间件==================
 	// 路由组使用 gin.BasicAuth() 中间件
@@ -334,34 +120,11 @@ func main() {
 	globalvar.Route.Any("/testing", myHandler.StartPage)
 
 	// 当在中间件或 handler 中启动新的 Goroutine 时，不能使用原始的上下文，必须使用只读副本。
-	globalvar.Route.GET("/long_async", func(c *gin.Context) {
-		// 创建在 goroutine 中使用的副本
-		cCp := c.Copy()
-		go func() {
-			// 用 time.Sleep() 模拟一个长任务。
-			time.Sleep(5 * time.Second)
+	globalvar.Route.GET("/longasync", myHandler.LongAsync)
 
-			// 请注意您使用的是复制的上下文 "cCp"，这一点很重要
-			log.Println("Done! in path " + cCp.Request.URL.Path)
-		}()
-	})
+	globalvar.Route.GET("/longsync", myHandler.LongSync)
 
-	globalvar.Route.GET("/long_sync", func(c *gin.Context) {
-		// 用 time.Sleep() 模拟一个长任务。
-		time.Sleep(5 * time.Second)
-
-		// 因为没有使用 goroutine，不需要拷贝上下文
-		log.Println("Done! in path " + c.Request.URL.Path)
-	})
-
-	globalvar.Route.GET("/:name/:id", func(c *gin.Context) {
-		var person types.Person
-		if err := c.ShouldBindUri(&person); err != nil {
-			c.JSON(400, gin.H{"msg": err.Error()})
-			return
-		}
-		c.JSON(200, gin.H{"name": person.Name, "uuid": person.ID})
-	})
+	globalvar.Route.GET("/:name/:id", myHandler.GetDataByUri)
 
 	// 传统启动服务器
 	// r.RunTLS(":443", "./cert/server.pem", "./cert/server.key")
--- gin/handler/basic.go
+++ gin/handler/basic.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/qinchy/hellogo/gin/globalvar"
 	"github.com/qinchy/hellogo/gin/types"
+	"github.com/qinchy/hellogo/pkg/proto"
 	"log"
 	"net/http"
+	"time"
 )
 
 func Ping(c *gin.Context) {
@@ -34,4 +37,280 @@ func Getting(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
 	}
+}
+
+func SomeJson(c *gin.Context) {
+	data := map[string]interface{}{
+		"lang": "GO语言",
+		"tag":  "<br>",
+	}
+
+	// 输出 : {"lang":"GO\u8bed\u8a00","tag":"\u003cbr\u003e"}
+	c.AsciiJSON(http.StatusOK, data)
+}
+
+func MoreJson(c *gin.Context) {
+	// 你也可以使用一个结构体
+	var msg struct {
+		Name    string `json:"user"`
+		Message string
+		Number  int
+	}
+	msg.Name = "Lena"
+	msg.Message = "hey"
+	msg.Number = 123
+	// 注意 msg.Name 在 JSON 中变成了 "user"
+	// 将输出：{"user": "Lena", "Message": "hey", "Number": 123}
+	c.JSON(http.StatusOK, msg)
+}
+
+func SomeXml(c *gin.Context) {
+	c.XML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
+}
+
+func SomeYaml(c *gin.Context) {
+	c.YAML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
+}
+
+func SomeProtoBuf(c *gin.Context) {
+	reps := []int64{int64(1), int64(2)}
+	label := "test"
+	// protobuf 的具体定义写在 pkg/proto 文件中。
+	data := &proto.Test{
+		Label: &label,
+		Reps:  reps,
+	}
+	// 请注意，数据在响应中变为二进制数据
+	// 将输出被 proto.Test protobuf 序列化了的数据
+	c.ProtoBuf(http.StatusOK, data)
+}
+
+func Index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title": "Main website",
+	})
+}
+
+func PostIndex(c *gin.Context) {
+	c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{
+		"title": "Posts",
+	})
+}
+
+func UsersIndex(c *gin.Context) {
+	c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
+		"title": "Users",
+	})
+}
+
+func JsonP(c *gin.Context) {
+	data := map[string]interface{}{
+		"foo": "bar",
+	}
+
+	// /JSONP?callback=x
+	// 将输出：x({\"foo\":\"bar\"})
+	c.JSONP(http.StatusOK, data)
+}
+
+func Json(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"html": "<b>Hello, world!</b>",
+	})
+}
+
+func SecureJson(c *gin.Context) {
+	names := []string{"lena", "austin", "foo"}
+
+	// 将输出：while(1);["lena","austin","foo"]
+	c.SecureJSON(http.StatusOK, names)
+}
+
+func LoginForm(c *gin.Context) {
+	// 你可以使用显式绑定声明绑定 multipart form：
+	// c.ShouldBindWith(&form, binding.Form)
+	// 或者简单地使用 ShouldBind 方法自动绑定：
+	var form types.LoginForm
+	// 在这种情况下，将自动选择合适的绑定
+	if c.ShouldBind(&form) == nil {
+		if form.User == "user" && form.Password == "password" {
+			c.JSON(200, gin.H{"status": "you are logged in"})
+		} else {
+			c.JSON(401, gin.H{"status": "unauthorized"})
+		}
+	}
+}
+
+func BindForm(c *gin.Context) {
+	var person types.Person
+	// 如果是 `GET` 请求，只使用 `Form` 绑定引擎（`query`）。
+	// 如果是 `POST` 请求，首先检查 `content-type` 是否为 `JSON` 或 `XML`，然后再使用 `Form`（`form-data`）。
+	// 查看更多：https://github.com/gin-gonic/gin/blob/master/binding/binding.go#L88
+	if c.ShouldBind(&person) == nil {
+		log.Println(person.Name)
+		log.Println(person.Address)
+		log.Println(person.Birthday)
+		c.String(200, "Success")
+	} else {
+		c.String(400, "Error")
+	}
+}
+
+func LoginJson(c *gin.Context) {
+	var json types.LoginForm
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if json.User != "user" || json.Password != "password" {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+}
+
+func LoginXml(c *gin.Context) {
+	var xml types.LoginForm
+	if err := c.ShouldBindXML(&xml); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if xml.User != "user" || xml.Password != "password" {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+}
+
+func PostForm(c *gin.Context) {
+	message := c.PostForm("message")
+	nick := c.DefaultPostForm("nick", "anonymous")
+
+	c.JSON(200, gin.H{
+		"status":  "posted",
+		"message": message,
+		"nick":    nick,
+	})
+}
+
+func PostFormWithQuery(c *gin.Context) {
+	// 请求示例
+	// POST /post?id=1234&page=1 HTTP/1.1
+	// Content-Type: application/x-www-form-urlencoded
+	//name=manu&message=this_is_great
+
+	// 从url中获取字段
+	id := c.Query("id")
+	page := c.DefaultQuery("page", "0")
+	// 从表单中获取字段
+	name := c.PostForm("name")
+	message := c.PostForm("message")
+
+	fmt.Printf("id: %s; page: %s; name: %s; message: %s", id, page, name, message)
+	c.JSON(200, gin.H{
+		"id":      id,
+		"page":    page,
+		"name":    name,
+		"message": message,
+	})
+}
+
+func PostMultiFormWithQuery(c *gin.Context) {
+	ids := c.QueryMap("ids")
+	names := c.PostFormMap("names")
+
+	log.Printf("ids: %v; names: %v", ids, names)
+	c.JSON(200, gin.H{
+		"ids":  ids,
+		"name": names,
+	})
+}
+
+func PureJson(c *gin.Context) {
+	// 这里的sleep是用来模拟优雅关机的，当请求来后，会处理完这个请求后再关闭服务器
+	time.Sleep(10 * time.Second)
+	c.PureJSON(200, gin.H{
+		"html": "<b>Hello, world!</b>",
+	})
+}
+
+func SingleUpload(c *gin.Context) {
+	// 单文件
+	file, _ := c.FormFile("file")
+	log.Println(file.Filename)
+
+	// 这里的当前目录是整个工程的路径，比如当前是$GOPATH/src/github.com/qinchy/hellogo
+	// 感觉这个目录是go.mod中module的路径
+	dst := "./" + file.Filename
+	log.Printf("dst:%v", dst)
+	// 上传文件至指定的完整文件路径
+	c.SaveUploadedFile(file, dst)
+
+	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+}
+
+func MultiUpload(c *gin.Context) {
+	// Multipart form
+	form, _ := c.MultipartForm()
+	files := form.File["upload[]"]
+
+	for _, file := range files {
+		log.Println(file.Filename)
+
+		dst := "./" + file.Filename + ".bak"
+		// 上传文件至指定目录
+		c.SaveUploadedFile(file, dst)
+	}
+	c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
+}
+
+func FetchFromReader(c *gin.Context) {
+	response, err := http.Get("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png")
+	if err != nil || response.StatusCode != http.StatusOK {
+		c.Status(http.StatusServiceUnavailable)
+		return
+	}
+
+	reader := response.Body
+	contentLength := response.ContentLength
+	contentType := response.Header.Get("Content-Type")
+
+	extraHeaders := map[string]string{
+		"Content-Disposition": `attachment; filename="gopher.png"`,
+	}
+
+	c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
+}
+
+func LongAsync(c *gin.Context) {
+	// 创建在 goroutine 中使用的副本
+	cCp := c.Copy()
+	go func() {
+		// 用 time.Sleep() 模拟一个长任务。
+		time.Sleep(5 * time.Second)
+
+		// 请注意您使用的是复制的上下文 "cCp"，这一点很重要
+		log.Println("Done! in path " + cCp.Request.URL.Path)
+	}()
+}
+
+func LongSync(c *gin.Context) {
+	// 用 time.Sleep() 模拟一个长任务。
+	time.Sleep(5 * time.Second)
+
+	// 因为没有使用 goroutine，不需要拷贝上下文
+	log.Println("Done! in path " + c.Request.URL.Path)
+}
+
+func GetDataByUri(c *gin.Context) {
+	var person types.Person
+	if err := c.ShouldBindUri(&person); err != nil {
+		c.JSON(400, gin.H{"msg": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"name": person.Name, "uuid": person.ID})
 }
